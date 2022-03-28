3134d70b8f802f6b7ac48bf6970db5d4302d2c01
Vincent <vshan0816@gmail.com>
2022-03-28 13:52:07 -0400

    fill out AnimeCard component with fetch information

--- src/components/AnimeCard.js
+++ src/components/AnimeCard.js
@@ -1,8 +1,16 @@
 import React from 'react'
 
-const AnimeCard = () => {
+const AnimeCard = ({anime}) => {
   return (
-    <div>AnimeCard</div>
+    <div style={{border:"solid", width: "17 %", height:"25%", margin:"auto"}}>
+      <h3>{anime.title}</h3>
+      <img src = {anime.image_url} alt={anime.title} />
+      <h2>Synopsis</h2>
+      <p>{anime.synopsis}</p>
+      <h2>Release Year:{anime.release_year}</h2>
+      <h2>Total Seasons:{anime.total_seasons}</h2>
+      <h2>Studio:{anime.studio.name}</h2>
+    </div>
   )
 }
 
