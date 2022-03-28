3705de99c482f069ed93e5c4aaf299a5bed87fe1
Vincent <vshan0816@gmail.com>
2022-03-28 13:28:57 -0400

    create fetch call in AnimesContainer, return Animelist component

--- src/containers/AnimesContainer.js
+++ src/containers/AnimesContainer.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import {useState, useEffect} from 'react'
+import AnimeList from '../components/AnimeList'
 
 const AnimesContainer = () => {
+  const [animes, setAnimes] = useState([])
+  const fetchData = () => {
+      fetch("http://localhost:9393/animes")
+      .then(response => response.json())
+      .then(data => setAnimes(data))
+      .catch(error => alert(error))
+  }
+
+  useEffect(() => {
+      fetchData()
+  }, [])
   return (
-    <div>AnimesContainer</div>
+    <div>
+      <AnimeList animes={animes}/>
+    </div>
   )
 }
 
