79cffa93817c58d9f9bfa4359599682c10cb3001
Vincent <vshan0816@gmail.com>
2022-03-28 14:24:43 -0400

    Fill in information for studios list and studios container

--- src/components/StudioList.js
+++ src/components/StudioList.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import StudioCard from './StudioCard'
 
-const StudioList = () => {
+const StudioList = ({studios}) => {
+  const StudiosCard = studios.map(studio => {
+    return <StudioCard key={studio.id} studio={studio} />
+})
   return (
-    <div>StudioList</div>
+    <div>
+       <h2>Studios</h2>
+            <div style ={{display: "flex", flexWrap: "wrap"}}> 
+                {StudiosCard}
+            </div>
+    </div>
   )
 }
 
--- src/containers/StudiosContainer.js
+++ src/containers/StudiosContainer.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import {useState, useEffect} from 'react'
+import StudioList from '../components/StudioList'
 
 const StudiosContainer = () => {
+  const [studios, setStudios] = useState([])
+  const fetchData = () => {
+      fetch("http://localhost:9393/studios")
+      .then(response => response.json())
+      .then(data => setStudios(data))
+      .catch(error => alert(error))
+  }
+
+  useEffect(() => {
+      fetchData()
+  }, [])
   return (
-    <div>StudiosContainer</div>
+    <div>
+      <StudioList studios={studios}/>
+    </div>
   )
 }
 
