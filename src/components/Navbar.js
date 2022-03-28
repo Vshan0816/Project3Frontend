0055142c2ff43787bea650fb76327332e91762f4
Vincent <vshan0816@gmail.com>
2022-03-28 12:30:26 -0400

    import Navlink, and create styles for Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,14 @@
-import React from 'react'
+import {NavLink} from 'react-router-dom'
+
+const style = {
+    width: "60%",
+    margin: "5% 0 1%",
+    padding: "1em",
+    textDecoration: "none",
+    color: "black",
+    fontweight: "bold",
+    verticalAlign: "center"
+}
 
 const Navbar = () => {
   return (
