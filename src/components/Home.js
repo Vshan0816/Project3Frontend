43c65ea22166ee7b4b6af63dce65f014896b240f
Vincent <vshan0816@gmail.com>
2022-03-30 13:08:11 -0400

    fill out homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+        <br></br>
+        <h2>Anime Project</h2>
+        <p>Feel free to browse through the current list of animes and studios and customize your list </p>
+        <br></br>
+        <img src = "https://i.pinimg.com/originals/99/97/77/99977763562005c4a64c66df5b2895a6.jpg" alt="anime" />
+    </div>
   )
 }
 
